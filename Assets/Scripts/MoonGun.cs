f33cdbf6cf7e596627dc2166f8dd43dba678c916
thaDameer <thaDameer@users.noreply.github.com>
2020-03-21 14:23:08 +0100

    Moongun

---
+++ Assets/Scripts/MoonGun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonGun : MonoBehaviour
+{
+    public MoonScript moonPrefab;
+    public PlayerLogic playerLogic;
+  
+    public float shootSpeed;
+    [SerializeField]
+    Transform shootPos;
+    MoonScript activeMoon;
+    bool holdingShootButton;
+    bool hasShot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        holdingShootButton = Input.GetButton("Fire1");
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootMoon();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (hasShot && holdingShootButton)
+        {
+            activeMoon.MovingForward();
+        }
+    }
+
+    void ShootMoon()
+    {
+        if (!activeMoon)
+        {
+            hasShot = true;
+            activeMoon = Instantiate(moonPrefab, shootPos.transform.position, transform.rotation);
+        }
+    }
+}
