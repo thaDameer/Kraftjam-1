180a502a9ab565ba549dfb44228c9493fdfd6ddd
Huggarnn <fabian.hugert@gmail.com>
2020-03-21 22:29:57 +0100

    Button and Door mechanic

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField]
+    private int _openCondition;
+    [SerializeField]
+    private int _numberOfObjects = 0;
+    [SerializeField]
+    private bool _isOpen = false;
+    [SerializeField]
+    private Door door; 
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Object")
+        {
+            _numberOfObjects++; 
+
+            if(_numberOfObjects >= _openCondition)
+            {
+                _isOpen = true;
+
+                door.OpenDoor(); 
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Object")
+        {
+            _numberOfObjects--; 
+
+            if(_numberOfObjects < _openCondition)
+            {
+                _isOpen = false;
+
+                door.CloseDoor(); 
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Door : MonoBehaviour
+{
+
+    public void OpenDoor()
+    {
+        transform.DORotate(new Vector3(0, 120, 0), 2); 
+    }
+
+    public void CloseDoor()
+    {
+        transform.DORotate(new Vector3(0, 0, 0), 2);
+    }
+
+}
